49a4f0bd5ad797315b2b1172d2197b941c081fae
Andrey-Abramovich <aan_84@tut.by>
2023-01-09 08:21:13 +0300

    change url start page

--- djangofinal/urls.py
+++ djangofinal/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profil/', include('profil.urls')),
-    path('board/', include('board.urls'))
+    path('', include('board.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
